b24bb4fc777ef6f0b7e35165e69a63207b0b5b80
Peti <antalpeti@users.noreply.github.com>
2017-02-06 13:29:45 +0100

    Process Cursors examples. #12

---
+++ cursors/implicit.sql
@@ -0,0 +1,18 @@
+DECLARE 
+   total_rows number(2);
+BEGIN
+   UPDATE customers
+   SET salary = salary + 500;
+   IF sql%notfound THEN
+      dbms_output.put_line('no customers selected');
+   ELSIF sql%found THEN
+      total_rows := sql%rowcount;
+      dbms_output.put_line( total_rows || ' customers selected ');
+   END IF; 
+   IF sql%isopen THEN
+      dbms_output.put_line( 'Implicit cursor is open. ');
+   ELSE
+      dbms_output.put_line( 'Implicit cursor is closed. ');
+   END IF;
+END;
+/
