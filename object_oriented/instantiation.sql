ef54d99ac4084ed48c8d5de3e26a0b47467def35
Peti <antalpeti@users.noreply.github.com>
2017-02-08 16:51:52 +0100

    Process Object Oriented examples. #21

---
+++ object_oriented/instantiation.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE TYPE address AS OBJECT
+(house_no varchar2(10),
+ street varchar2(30),
+ city varchar2(20),
+ state varchar2(10),
+ pincode varchar2(10)
+);
+/
+
+CREATE OR REPLACE TYPE customer AS OBJECT
+(code number(5),
+ name varchar2(30),
+ contact_no varchar2(12),
+ addr address,
+ member procedure display
+);
+/
+
+DECLARE
+   residence address;
+BEGIN
+   residence := address('103A', 'M.G.Road', 'Jaipur', 'Rajasthan','201301');
+   dbms_output.put_line('House No: '|| residence.house_no);
+   dbms_output.put_line('Street: '|| residence.street);
+   dbms_output.put_line('City: '|| residence.city);
+   dbms_output.put_line('State: '|| residence.state);
+   dbms_output.put_line('Pincode: '|| residence.pincode);
+END;
+/
